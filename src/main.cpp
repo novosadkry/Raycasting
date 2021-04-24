04a35c20a2944fd1c338e8b2be5db48415513698
Kryštof Novosad <novosadkry@seznam.cz>
2021-04-24 18:44:27 +0200

    Add SFML to project libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-#include "hello/hello.h"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    hello();
-    return 0;
+	sf::RenderWindow rw(sf::VideoMode(800, 600), "SFML window");
+
+	while (rw.isOpen())
+	{
+		sf::Event e{};
+		
+		while (rw.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed)
+				rw.close();
+		}
+	}
+	
+    return EXIT_SUCCESS;
 }
