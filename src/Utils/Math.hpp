8bbb08cb22895f3bc57dacb294936815cc6dde97
Kryštof Novosad <novosadkry@seznam.cz>
2021-06-30 18:38:58 +0200

    Add generic vector functions

--- src/Utils/Math.hpp
+++ src/Utils/Math.hpp
@@ -14,8 +14,40 @@ namespace Math
     inline sf::Vector2f Angle2Vector(float angle)
     {
         return sf::Vector2f(
-            cos(angle),
-            sin(angle)
+            cosf(angle),
+            sinf(angle)
         );
+    }
+
+    inline float Vector2Angle(sf::Vector2f vector)
+    {
+        return atan2f(vector.y, vector.x);
+    }
+
+    inline float Length(const sf::Vector2f& vector)
+    {
+        return sqrtf(vector.x * vector.x + vector.y * vector.y);
+    }
+
+    inline float LengthSqr(const sf::Vector2f& vector)
+    {
+        return vector.x * vector.x + vector.y * vector.y;
+    }
+
+    inline float Distance(const sf::Vector2f& v1, const sf::Vector2f& v2)
+    {
+        return Length(v1 - v2);
+    }
+
+    inline float DistanceSqr(const sf::Vector2f& v1, const sf::Vector2f& v2)
+    {
+        return LengthSqr(v1 - v2);
+    }
+
+    inline void Normalize(sf::Vector2f& vector)
+    {
+        float length = Length(vector);
+        vector.x /= length;
+        vector.y /= length;
     }
 }
