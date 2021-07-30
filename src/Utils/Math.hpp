aac78635f2bc9ea77faa024651dbbdd3f63cf94c
Kryštof Novosad <novosadkry@seznam.cz>
2021-07-30 14:55:37 +0200

    Implement Dot and Reflect functions

--- src/Utils/Math.hpp
+++ src/Utils/Math.hpp
@@ -17,19 +17,19 @@ namespace Math
         );
     }
 
-    inline float Vector2Angle(sf::Vector2f vector)
+    inline float Vector2Angle(const sf::Vector2f& v)
     {
-        return atan2(vector.y, vector.x);
+        return atan2(v.y, v.x);
     }
 
-    inline float Length(const sf::Vector2f& vector)
+    inline float Length(const sf::Vector2f& v)
     {
-        return sqrt(vector.x * vector.x + vector.y * vector.y);
+        return sqrt(v.x * v.x + v.y * v.y);
     }
 
-    inline float LengthSqr(const sf::Vector2f& vector)
+    inline float LengthSqr(const sf::Vector2f& v)
     {
-        return vector.x * vector.x + vector.y * vector.y;
+        return v.x * v.x + v.y * v.y;
     }
 
     inline float Distance(const sf::Vector2f& v1, const sf::Vector2f& v2)
@@ -42,10 +42,20 @@ namespace Math
         return LengthSqr(v1 - v2);
     }
 
-    inline void Normalize(sf::Vector2f& vector)
+    inline void Normalize(sf::Vector2f& v)
     {
-        float length = Length(vector);
-        vector.x /= length;
-        vector.y /= length;
+        float length = Length(v);
+        v.x /= length;
+        v.y /= length;
+    }
+
+    inline float Dot(const sf::Vector2f& v1, const sf::Vector2f& v2)
+    {
+        return v1.x * v2.x + v1.y * v2.y;
+    }
+
+    inline sf::Vector2f Reflect(const sf::Vector2f& v, const sf::Vector2f& n)
+    {
+        return v - 2.0f * Dot(n, v) * n;
     }
 }
