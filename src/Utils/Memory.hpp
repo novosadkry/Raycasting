08d300ad2e8a3e15d028fd56ac83a2de925d8060
Kryštof Novosad <novosadkry@seznam.cz>
2021-08-31 21:53:05 +0200

    Add missing weak_ptr alias

--- src/Utils/Memory.hpp
+++ src/Utils/Memory.hpp
@@ -1,8 +1,9 @@
 #pragma once
 #include <Core.hpp>
 
-template<typename T> using Unique = std::unique_ptr<T>;
-template<typename T> using Shared = std::shared_ptr<T>;
+template<typename T> using Unique   = std::unique_ptr<T>;
+template<typename T> using Shared   = std::shared_ptr<T>;
+template<typename T> using Weak     = std::weak_ptr<T>;
 
 template<typename T, typename... Args>
 inline Unique<T> MakeUnique(Args&&... args)
@@ -14,4 +15,10 @@ template<typename T, typename... Args>
 inline Shared<T> MakeShared(Args&&... args)
 {
     return std::make_shared<T>(std::forward<Args>(args)...);
+}
+
+template<typename T, typename... Args>
+inline Weak<T> MakeWeak(Args&&... args)
+{
+    return std::weak_ptr<T>(std::forward<Args>(args)...);
 }
