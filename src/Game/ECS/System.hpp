ab1765c13a37424328827bc2070f52d8780cfc26
Kryštof Novosad <novosadkry@seznam.cz>
2021-09-28 22:54:59 +0200

    Add SystemGroup

--- src/Game/ECS/System.hpp
+++ src/Game/ECS/System.hpp
@@ -18,4 +18,40 @@ namespace ECS
     protected:
         entt::registry* m_Registry;
     };
+
+    class SystemGroup
+    {
+    public:
+        SystemGroup(entt::registry* registry)
+            : m_Registry(registry) { }
+
+        template<typename T>
+        void Add()
+        {
+            m_Systems.emplace_back(MakeUnique<T>(m_Registry));
+        }
+
+        template<typename T>
+        void Remove()
+        {
+            for (auto it = m_Systems.begin(); it != m_Systems.end(); it++)
+            {
+                if (dynamic_cast<T*>(it->get()))
+                {
+                    it = m_Systems.erase(it);
+                    return;
+                }
+            }
+        }
+
+        void Update(float dt)
+        {
+            for (auto& system : m_Systems)
+                system->Update(dt);
+        }
+
+    private:
+        entt::registry* m_Registry;
+        std::list<Unique<System>> m_Systems;
+    };
 }
