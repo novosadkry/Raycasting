56e8027c9ad96c339c74e68717595a01160a57a0
Kryštof Novosad <novosadkry@seznam.cz>
2021-10-03 12:33:03 +0200

    Add cereal to project libs

--- src/Rpch.hpp
+++ src/Rpch.hpp
@@ -10,6 +10,17 @@
 
 #include <entt/entt.hpp>
 
+#include <cereal/archives/xml.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/archives/binary.hpp>
+
+#include <cereal/types/map.hpp>
+#include <cereal/types/vector.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/memory.hpp>
+#include <cereal/types/common.hpp>
+#include <cereal/types/complex.hpp>
+
 #include <list>
 #include <stack>
 #include <deque>
